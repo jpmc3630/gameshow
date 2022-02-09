c76a88e1302dbdbbc293f3d46c46618b987c5c7e
James McGlone <jamespmcglone@gmail.com>
2022-02-10 02:10:37 +0800

    Add auth pusher channel

--- routes/channels.php
+++ routes/channels.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('my-channel', function ($user) {
+    // return (int) $user->id === (int) $id;
+    \Log::info('my-channel');
+    if (Auth::check()) {
+        \Log::info('user is authed');      
+    }
+    return true;
 });
+
+// Broadcast::channel('user.{userId}', function ($user, $userId) {
+//   return $user->id === $userId;
+// });
