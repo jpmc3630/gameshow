1c2ca67e9c73fe7efffa13770fe7f8e3d12158c6
James McGlone <jamespmcglone@gmail.com>
2022-03-07 05:10:34 +0800

    Use room field from user model for channel

--- routes/channels.php
+++ routes/channels.php
@@ -13,13 +13,20 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::channel('my-channel', function ($user) {
+Broadcast::channel('{userRoom}', function ($user) {
+  // \Log::info('user: ' + $user);
+// Broadcast::channel('KUMY', function ($user) {
     // return (int) $user->id === (int) $id;
-    \Log::info('my-channel');
-    if (Auth::check()) {
-        \Log::info('user is authed');      
-    }
-    return true;
+    \Log::info('Channel is called: ' . $user->room);
+    // if (Auth::check()) {
+    //     \Log::info('user is authed');      
+    // }
+
+
+    // return true; // for priveate channel
+
+    return ['id' => $user->id, 'name' => $user->name];
+    // return ['id' => 3, 'name' => 'John'];
 });
 
 // Broadcast::channel('user.{userId}', function ($user, $userId) {
